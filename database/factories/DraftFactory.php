ea3a397d3f0a5d5fa3cf4e677b0f6f7e82c3777e
n-ull <felix.greed@gmail.com>
2023-09-02 22:15:48 -0300

    Draft and Category Factory

---
+++ database/factories/DraftFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Draft>
+ */
+class DraftFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'title' => $this->faker->words(2),
+            'description' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'content' => $this->faker->paragraphs(3, true),
+        ];
+    }
+}
