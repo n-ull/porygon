8966b738f810aca00505562c7ae12b0bd7f48c73
n-ull <felix.greed@gmail.com>
2023-09-12 23:37:42 -0300

    markdown parser

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,46 +3,4 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-
-const theme = localStorage.getItem('theme') ?? 'system'
-
-window.Alpine.store(
-    'theme',
-    theme === 'dark' ||
-        (theme === 'system' &&
-            window.matchMedia('(prefers-color-scheme: dark)').matches)
-        ? 'dark'
-        : 'light',
-)
-
-window.addEventListener('theme-changed', (event) => {
-    let theme = event.detail
-
-    localStorage.setItem('theme', theme)
-
-    if (theme === 'system') {
-        theme = window.matchMedia('(prefers-color-scheme: dark)').matches
-            ? 'dark'
-            : 'light'
-    }
-
-    window.Alpine.store('theme', theme)
-})
-
-window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', (event) => {
-        if (localStorage.getItem('theme') === 'system') {
-            window.Alpine.store('theme', event.matches ? 'dark' : 'light')
-        }
-    })
-
-window.Alpine.effect(() => {
-    const theme = window.Alpine.store('theme')
-
-    theme === 'dark'
-        ? document.documentElement.classList.add('dark')
-        : document.documentElement.classList.remove('dark')
-})
-
 Alpine.start();
--- resources/views/draft/show.blade.php
+++ resources/views/draft/show.blade.php
@@ -13,16 +13,34 @@
             @else
                 <h1 class="text-white z-10">{{ $draft->title }}</h1>
             @endisset
-
         </div>
         <div class="p-6">
             <div class="grid grid-cols-2">
-                <div>{{ Markdown::parse($draft->content) }}</div>
+                @markdown($draft->content)
                 <div class="justify-self-center">Images</div>
             </div>
+
+            <div x-data="{ open: false }" class="mt-4">
+                <button @click="open = !open" class="bg-gray-300/30 w-full p-2">
+                    Show more info
+                </button>
+                <div x-show="open" x-transition:enter="transition ease-out origin-top-left duration-200"
+                    x-transition:enter-start="opacity-0 transform scale-90"
+                    x-transition:enter-end="opacity-100 transform scale-100"
+                    x-transition:leave="transition origin-top-left ease-in duration-100"
+                    x-transition:leave-start="opacity-100 transform scale-100"
+                    x-transition:leave-end="opacity-0 transform scale-90" class="p-4 bg-gray-300/75 text-left">
+                    <ul>
+                        <li>Published: <x-heroicon-o-clock class="inline-block h-4 w-4" /> {{$draft->created_at->diffForHumans()}}</li>
+                        <li>Category: {{$draft->category->name ?? 'None'}}</li>
+                        <li>Author: <a href="{{route('profile.visit', $draft->user)}}" class="text-secondary hover:underline">&#64;{{$draft->user->username}}</a></li>
+                    </ul>
+                </div>
+            </div>
+
             <div class="mt-4">
                 @if ($draft->shards->count() > 0)
-                <div class="bg-gray-300/30 p-2">Draft Shard</div>
+                    <div class="bg-gray-300/30 p-2">Draft Shard</div>
                 @else
                     <div class="bg-gray-300/30 text-gray-400/50 p-2">This project doesn't have any shard...</div>
                 @endif
